727cd701637ad8b082195a8562a9563dadd2c5ec
RCx02 <93350632+RCx02@users.noreply.github.com>
2021-11-26 11:57:20 -0800

    Push

---
+++ Assets/Scripts/Player/PlayerMovementV2.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class PlayerMovementV2 : MonoBehaviour
+{
+    public float defaultSpeed = 7.5f,runningSpeed = 11.5f,jumpForce = 8.0f,gravity = 20.0f;
+
+    public Camera playerCamera;
+    CharacterController characterController;
+    Vector3 moveDirection = Vector3.zero;
+
+    [HideInInspector]
+    public bool canMove = true;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+        float speedX = canMove ? (defaultSpeed) * Input.GetAxis("Vertical") : 0;
+        float speedY = canMove ? (defaultSpeed) * Input.GetAxis("Horizontal") : 0;
+        float movementDirectionY = moveDirection.y;
+        moveDirection = (forward * speedX) + (right * speedY);
+        if (!characterController.isGrounded)
+        {
+            moveDirection.y -= gravity * Time.deltaTime;
+        }
+
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+} 
